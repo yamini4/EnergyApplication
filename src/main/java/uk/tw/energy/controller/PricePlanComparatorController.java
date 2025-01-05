d7a33de2fac07d2b55752d5485c473b4e4ad60e9
yamini <yamini4.g@gmail.com>
2025-01-05 16:44:19 +0530

    dummy changes

--- src/main/java/uk/tw/energy/controller/PricePlanComparatorController.java
+++ src/main/java/uk/tw/energy/controller/PricePlanComparatorController.java
@@ -58,6 +58,7 @@ public class PricePlanComparatorController {
         if (!consumptionsForPricePlans.isPresent()) {
             return ResponseEntity.notFound().build();
         }
+        
 
         List<Map.Entry<String, BigDecimal>> recommendations =
                 new ArrayList<>(consumptionsForPricePlans.get().entrySet());
