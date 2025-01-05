4dd17082ae2912a28723854b43630bfe55a03df8
yamini <yamini4.g@gmail.com>
2025-01-05 16:39:10 +0530

    initialization

---
+++ src/main/java/uk/tw/energy/Energy.java
@@ -0,0 +1,11 @@
+package uk.tw.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Energy {
+	public static void main(String[] args) {
+		SpringApplication.run(Energy.class, args);
+	}
+}
